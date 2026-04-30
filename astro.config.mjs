cfbb92a2eba96302d1133c20e41c4f389c876c4f
nathanEriksen <nathanky99@gmail.com>
2026-04-30 12:53:44 -0400

    astro config changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
-// @ts-check
+// // @ts-check
+// import { defineConfig } from 'astro/config';
+
+// // https://astro.build/config
+// export default defineConfig({});
+
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://nathan-707.github.io',
+    base: '/Portfolio',
+});
